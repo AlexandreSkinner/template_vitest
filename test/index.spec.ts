a32c4ef4c64bdbc59c3b73ab612dc286a8bc14b7
Alexandre Skinner <AlexandreSkinner@tmassessoria.com.br>
2023-07-12 16:40:22 -0300

    test: adiciona operação de multiplicação e subtração

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,8 +1,15 @@
+import { describe, test, expect } from 'vitest';
 import Person from '@/index';
 
 describe('teste operacoes aritmeticas', () => {
   test('testa soma', () => {
     expect(1 + 2).toBe(3);
+  });
+  test('testa subtração', () => {
+    expect(3 - 2).toBe(1);
+  });
+  test('testa multiplicação', () => {
+    expect(3 * 2).toBe(6);
   });
 });
 
